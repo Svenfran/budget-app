fdc4d2ce7b79ba3f43893c7ce1b7caca526754fa
SHI <sven.haider@qualityminds.de>
2022-08-19 13:37:13 +0200

    setup project

---
+++ spring-backend/src/test/java/com/github/svenfran/budgetapp/budgetappbackend/BudgetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.svenfran.budgetapp.budgetappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
