1b323276119e733acdb4bbd641be51df1780fc90
SHI <sven.haider@qualityminds.de>
2025-08-31 14:15:19 +0200

    BE: websocket alloworigin testweise auf * angepasst

--- spring-backend/src/main/java/com/github/svenfran/budgetapp/budgetappbackend/config/WebSocketConfig.java
+++ spring-backend/src/main/java/com/github/svenfran/budgetapp/budgetappbackend/config/WebSocketConfig.java
@@ -38,9 +38,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/wss")
-                .setAllowedOriginPatterns("http://localhost:8100", "http://192.168.178.23:8100",
-                        "http://localhost", "http://192.168.178.23", "https://localhost:8100",
-                        "https://localhost:", "https://divvy-app.up.railway.app")
+//                .setAllowedOriginPatterns("http://localhost:8100", "http://192.168.178.23:8100",
+//                        "http://localhost", "http://192.168.178.23", "https://localhost:8100",
+//                        "https://localhost:", "https://divvy-app.up.railway.app")
+                .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
 
